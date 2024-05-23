5fe5b0698fcda4d0648e583c83edf53184e327ed
esaucruzbird <166883671+esaucruzbird@users.noreply.github.com>
2024-05-22 20:29:51 -0600

    Funcion principal y aspecto grafico en el main

--- main.py
+++ main.py
@@ -29,3 +29,27 @@ def open_descifrar():
     subprocess.run(["python", "descifrar.py"])
 
 
+# Crea y establece el valor por defecto a 3 en el archivo de cifrado si no existe #
+def create_config_file():
+    config_file = "config.cfg"
+    if not os.path.exists(config_file):
+        with open(config_file, "w") as file:
+            file.write("cesar = 3\n")
+        messagebox.showinfo("Configuración creada", "Archivo de configuración de cifrado creado.")
+
+
+# Funcion principal y aspecto gráfico #
+def main_window():
+    root = tk.Tk()
+    root.iconbitmap('icono.png')
+    root.geometry("475x350")
+    logo_image = tk.PhotoImage(file='cifrado.png')
+    logo_image = logo_image.subsample(2, 2)
+    logo_label = tk.Label(root, image=logo_image)
+    logo_label.image = logo_image
+    logo_label.pack(pady=15)
+    root.title("Menú Principal - Cifrado Caesar")
+ 
+    cesar_label = tk.Label(root,  font='Helvetica 18 bold' ,text='Cifrado César')
+    cesar_label.pack(pady=10)
+
